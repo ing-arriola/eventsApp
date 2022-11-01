66b6c3fbe7dd70b7abb5922084a0e130ec433a7b
ing-arriola <ingeniero.arriola@gmail.com>
2022-11-01 08:13:23 -0600

    Added button

--- componets/ui/Button.tsx
+++ componets/ui/Button.tsx
@@ -1,15 +1,24 @@
 import Link from 'next/link';
+import { MouseEventHandler } from 'react';
 import styles from './Button.module.css';
 
 interface buttonProps {
   children: JSX.Element[] | JSX.Element | string;
-  link: string;
+  link?: string;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
-export const Button = ({ children, link }: buttonProps) => {
+export const Button = ({ children, link, onClick }: buttonProps) => {
+  if (link) {
+    return (
+      <Link href={link} className={styles.btn}>
+        {children}
+      </Link>
+    );
+  }
   return (
-    <Link href={link} className={styles.btn}>
+    <button className={styles.btn} onClick={onClick}>
       {children}
-    </Link>
+    </button>
   );
 };
