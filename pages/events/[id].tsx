f56b078b71cb29acf02a8b140c2b610a19288376
ing-arriola <ingeniero.arriola@gmail.com>
2022-11-01 08:42:40 -0600

    added logic to dynamic routes page

--- pages/events/[id].tsx
+++ pages/events/[id].tsx
@@ -1,7 +1,38 @@
 import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import {
+  EventContent,
+  EventLogistics,
+  EventSummary,
+} from '../../componets/event-detail';
+import { ErrorAlert } from '../../componets/ui';
+import { getEventById } from '../../dummy-data';
 
 const DetailsEventPage: NextPage = () => {
-  return <div>DetailsEventPage</div>;
+  const router = useRouter();
+  const eventId = router.query.id;
+  const event = getEventById(eventId);
+  if (!event) {
+    return (
+      <ErrorAlert>
+        <p>No events were found!!</p>
+      </ErrorAlert>
+    );
+  }
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.description}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
 };
 
 export default DetailsEventPage;
--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,7 +1,26 @@
 import { NextPage } from 'next';
+import { EventList } from '../../componets/events';
+import { EventsSearch } from '../../componets/events/EventsSearch';
+import { getAllEvents } from '../../dummy-data';
+import { useRouter } from 'next/router';
 
 const AllEventsPage: NextPage = () => {
-  return <div>AllEventsPage</div>;
+  const events = getAllEvents();
+  const router = useRouter();
+
+  const findEventsHandler = (
+    year: string | undefined,
+    month: string | undefined
+  ) => {
+    router.push(`/events/${year}/${month}`);
+  };
+
+  return (
+    <div>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList eventList={events} />
+    </div>
+  );
 };
 
 export default AllEventsPage;
